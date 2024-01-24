d50b21a27d4a2222a5e2ecd78ad5a399652bf2f9
Jypa <jyri.pappinen@gmail.com>
2024-01-24 19:17:03 +0200

    Api endpoint fixes after server changes

--- src/services/aikataulut.js
+++ src/services/aikataulut.js
@@ -56,11 +56,11 @@ const getStop = (stopId, oneOrThree) =>{
         }
         if(day !== 0){
             if( aika > "15:00:00"){
-                const request = axios.get(`https://data.itsfactory.fi/journeys/api/1/journeys?startIndex=90&stopPointId=${stopname}&daysTypes=${days[day]}&lineId=1`)
+                const request = axios.get(`https://data.itsfactory.fi/journeys/api/1/journeys?startIndex=70&stopPointId=${stopname}&dayTypes=${days[day]}&lineId=1`)
                 return request.then(response =>response.data)      
             }
             else{
-                const request = axios.get(`https://data.itsfactory.fi/journeys/api/1/journeys?stopPointId=${stopname}&daysTypes=${days[day]}&lineId=1`)
+                const request = axios.get(`https://data.itsfactory.fi/journeys/api/1/journeys?stopPointId=${stopname}&dayTypes=${days[day]}&lineId=1`)
                 return request.then(response =>response.data)
             }
         }   
